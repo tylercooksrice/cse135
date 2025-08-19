af70297d738632edcb103287bb46d37e8d5dfa3c
jvaladeus <asubbarao@ucsd.edu>
2025-08-19 23:26:31 +0000

    ~14% done

--- src/cgi-bin/php-hello-json-world.php
+++ src/cgi-bin/php-hello-json-world.php
@@ -1,6 +1,6 @@
 <?php
 $myObj = new stdClass();
-$myObj->message = "Hello World From PHP!";
+$myObj->message = "Hello World from PHP!";
 $myObj->date = date("D M d H:i:s Y");
 $myObj->ipAddress = getenv("REMOTE_ADDR");
 
