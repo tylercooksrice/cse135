9b7dffb433ea281a13b89b067fb0d6eafbb212b9
jvaladeus <asubbarao@ucsd.edu>
2025-08-19 22:06:15 +0000

    finished first php file

---
+++ src/cgi-bin/php-hello-html-world.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    print "<h1>Hello, PHP!</h1>";
+    print "<p>This page was generated with php</p>";
+    
+    $datestring = date("D M d H:i:s Y");
+    print "<p>Current Time: $datestring\n</p>";
+    $ipaddress = getenv("REMOTE_ADDR");
+    print "<p>your IP address is: $ipaddress\n</p>";
+    ?>
+</body>
+
+</html>
