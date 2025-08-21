5632739930529c4220aca6422bfcf5d043b14b34
tylercooksrice <darksilverarrow@gmail.com>
2025-08-21 15:18:16 -0700

    testing py hello

--- src/cgi-bin/py-hello-html-world.py
+++ src/cgi-bin/py-hello-html-world.py
@@ -3,12 +3,15 @@ import cgi
 import datetime
 import os
 
-print("Content-Type: text/html")
-print()
+print("Cache-Control: no-cache\n)
+print("Content-Type: text/html\n\n")
 
 print("""
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Hello Python World</title>
+</head>
 <body>
     <h1>Hello, Python!</h1>
     <p>This page was generated with Python!</p>
