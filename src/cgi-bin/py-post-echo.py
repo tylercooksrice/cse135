c2734e90080a457d7cbe9aee595e34cf50521698
tylercooksrice <darksilverarrow@gmail.com>
2025-08-24 18:17:36 -0700

    oopsies

--- src/cgi-bin/py-post-echo.py
+++ src/cgi-bin/py-post-echo.py
@@ -13,10 +13,22 @@ print("""
 <head>
     <title>Python Post</title>
 </head>""")
+print("<!DOCTYPE html>")
+print("""
+<html>
+<head>
+    <title>Python Post</title>
+</head>
+<body>
 print("<h1>POST Data</h1>")
-print("<ul>")
+print("<ul>")""")
 for key in form.keys():
     print(f"<li>{key} = {form.getvalue(key)}</li>")
+    
 print("</ul>")
-print("</body>")
-print("</html>")
+
+
+print("""
+</body>
+</html>
+""")
